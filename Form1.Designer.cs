7072a24a06a921536572cbbcfbee256e77eb82a9
Ravi Nayak <152935524+ravinayakk@users.noreply.github.com>
2023-12-04 20:04:09 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace LunchOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxMainCourse = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOrderTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSalesTax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxMainCourse
+            // 
+            this.gbxMainCourse.Location = new System.Drawing.Point(34, 17);
+            this.gbxMainCourse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbxMainCourse.Name = "gbxMainCourse";
+            this.gbxMainCourse.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbxMainCourse.Size = new System.Drawing.Size(159, 98);
+            this.gbxMainCourse.TabIndex = 0;
+            this.gbxMainCourse.TabStop = false;
+            this.gbxMainCourse.Text = "Main Course";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOrderTotal);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtSalesTax);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtSubtotal);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(34, 133);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(260, 115);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Order Total";
+            // 
+            // txtOrderTotal
+            // 
+            this.txtOrderTotal.Location = new System.Drawing.Point(112, 76);
+            this.txtOrderTotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOrderTotal.Name = "txtOrderTotal";
+            this.txtOrderTotal.ReadOnly = true;
+            this.txtOrderTotal.Size = new System.Drawing.Size(106, 23);
+            this.txtOrderTotal.TabIndex = 5;
+            this.txtOrderTotal.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Order Total:";
+            // 
+            // txtSalesTax
+            // 
+            this.txtSalesTax.Location = new System.Drawing.Point(112, 50);
+            this.txtSalesTax.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSalesTax.Name = "txtSalesTax";
+            this.txtSalesTax.ReadOnly = true;
+            this.txtSalesTax.Size = new System.Drawing.Size(106, 23);
+            this.txtSalesTax.TabIndex = 3;
+            this.txtSalesTax.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tax (7.75%):";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(112, 23);
+            this.txtSubtotal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.ReadOnly = true;
+            this.txtSubtotal.Size = new System.Drawing.Size(106, 23);
+            this.txtSubtotal.TabIndex = 1;
+            this.txtSubtotal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subtotal:";
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(320, 143);
+            this.btnPlaceOrder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(78, 25);
+            this.btnPlaceOrder.TabIndex = 2;
+            this.btnPlaceOrder.Text = "Place &Order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(320, 222);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(78, 25);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPlaceOrder;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(433, 274);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbxMainCourse);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lunch Order";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxMainCourse;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOrderTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalesTax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
